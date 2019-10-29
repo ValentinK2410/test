98c57ae2376d378957c1df0b2c70d3b362845da2
Valentink2410 <valentink2410@gmail.com>
2019-10-29 11:56:16 +0300

    added array and foreach js

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
-let a = "Hello, ";
+let arr = ["JavaScript", "PHP"];
 
 function test(txt) {
-    return "Hello, " + txt;
+  return "Hello, " + txt;
 }
+arr.forEach(function(item) {
+  console.log(test(item));
+});
 console.log(test("JavaScript!"));
 console.log(test("PHP!"));
